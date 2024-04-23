4ebd7ac6a0818d6a0535bceac4cfb983eeaedfd0
Charlice3612826 <146347928+Charlice3612826@users.noreply.github.com>
2024-04-23 23:25:38 +0800

    Update judge.cpp
    highlight choosemode

--- judge.cpp
+++ judge.cpp
@@ -1,6 +1,9 @@
-# include "gobang.h"
-# include <iostream>
+#include "gobang.h"
+#include <iostream>
 #include <vector>
+#include <string>
+#include <chrono>
+#include <thread>
 
 int judge(int x, int y) 
 {	
@@ -49,17 +52,72 @@ int judge(int x, int y)
 void ChooseMode()
 {	
 	string s;
-	printf("Please select the mode for\"pvp\"(p)or\"pve\"(a) or Residual situation(r):");
+	string text7_1 = "Please select the mode for\"pvp\"";
+	string text7_2 = "(p)or\"";
+	string text7_3 = "pve\"(a) or ";
+	string text7_3 = "Residual situation(r): ";
+
+	 for (char& c : text7_1) {
+        cout << BOLD << c << RESET;
+        this_thread::sleep_for(milliseconds(5));
+      }
+	for (char& c : text7_2) {
+        if (c == 'p'){
+	        cout << BOLD << RED << c << RESET;
+	        this_thread::sleep_for(milliseconds(5));
+	    }
+	    else {
+	        cout << BOLD << c << RESET;
+	        this_thread::sleep_for(milliseconds(5));
+	    }
+	}
+	for (char& c : text7_3) {
+        if (c == 'a'){
+	        cout << BOLD << RED << c << RESET;
+	        this_thread::sleep_for(milliseconds(5));
+	    }
+	    else {
+	        cout << BOLD << c << RESET;
+	        this_thread::sleep_for(milliseconds(5));
+	    }
+	}
+	for (char& c : text7_4) {
+        if (c == 'r'){
+	        cout << BOLD << RED << c << RESET;
+	        this_thread::sleep_for(milliseconds(5));
+	    }
+	    else {
+	        cout << BOLD << c << RESET;
+	        this_thread::sleep_for(milliseconds(5));
+	    }
+	}
 	cin >> s;
-	if(s[0] == 'p')
+	if(s == 'p')
 		mode = 0;
-	else if(s[0] == 'a')
+	else if(s == 'a')
 		mode = 1;
-	else if(s[0] == 'r')
+	else if(s == 'r')
 		mode = 2;
 	else
 	{
-		cout << "\nplease type in letter p or  a." << endl;
+		string text8_1 = "please type in letter ";
+		string text8_2 = "p or a."
+
+		for (char& c : text8_1) {
+		        cout << BOLD << c << RESET;
+		        this_thread::sleep_for(milliseconds(5));
+		      }
+
+		for (char& c : text8_2) {
+		        if (c == 'p' || c == 'a'){
+			        cout << BOLD << RED << c << RESET;
+			        this_thread::sleep_for(milliseconds(5));
+			    }
+			    else {
+			        cout << BOLD << c << RESET;
+			        this_thread::sleep_for(milliseconds(5));
+			    }
+			}
 		ChooseMode();
 	}
 } 
