1f1e5f1602139c612b6cb2098f1af4fdfd916690
tigressdddddd <159103807+tigressdddddd@users.noreply.github.com>
2024-04-24 19:34:17 +0800

    Update introduction.cpp

--- introduction.cpp
+++ introduction.cpp
@@ -17,11 +17,11 @@ void introduction() {
     for (char& c : title) {
         if (c == '$') {
             cout << BOLD << PINK << c << RESET;
-            this_thread::sleep_for(milliseconds(5));
+
         }
         else if (c == '_' || c == '\\' || c == '/' || c == '|') {
             cout << BOLD << BLUE << c << RESET;
-            this_thread::sleep_for(milliseconds(5));
+
         }
         else {
             cout << BOLD << RED << c << RESET;
